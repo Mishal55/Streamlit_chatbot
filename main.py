f7c05b5919294797e8c55b62995444b15f4a5505
MISHAL <160596015+Mishal55@users.noreply.github.com>
2025-07-01 07:10:53 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import requests
 
 # 🔐 Load API key securely from Streamlit Secrets
 api_key = st.secrets["OPENROUTER_API_KEY"]
-MODEL_NAME =  "openchat/openchat-3.5	"
-
+MODEL_NAME = "perplexity/pplx-7b-chat"
 # 🔧 Streamlit page config
 st.set_page_config(page_title="AI Chatbot", page_icon="🤖", layout="centered")
 st.markdown("<h2 style='text-align:center;'>🤖 Smart Chatbot </h2><hr>", unsafe_allow_html=True)
